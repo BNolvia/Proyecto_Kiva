feeef5beefa2a76d7b15401376fdd99cbaed8fb3
BNolvia <nolvia.castro@ujcv.edu.hn>
2017-02-17 13:06:24 -0600

    MainActivity para listviews

--- app/src/main/java/com/example/berenice/proyecto_kiva/MainActivity.java
+++ app/src/main/java/com/example/berenice/proyecto_kiva/MainActivity.java
@@ -3,6 +3,19 @@ package com.example.berenice.proyecto_kiva;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
     public static Context mContext;
@@ -11,7 +24,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext=this;
 
+        String url="http://api.kivaws.org/v1/loans/newest.json";
+
+        getKivaLoans(url);
 
     }
+
+    private void getKivaLoans(String url) {
+        final Context context=this;
+        JsonObjectRequest jor=new JsonObjectRequest(
+                url,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Logger.getAnonymousLogger().log(Level.INFO,response.toString());
+                        try {
+                            JSONArray loans=response.getJSONArray("loans");
+
+                            ArrayList<JSONObject> dataSourse=new ArrayList<JSONObject>();
+                            for(int i=0;i<loans.length();i++)
+                            {
+                                dataSourse.add(loans.getJSONObject(i));
+
+                            }
+                            CeldaAdapter adapter=new CeldaAdapter(context,0,dataSourse);
+                            ((ListView)findViewById(R.id.lista)).setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+
+                        }
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Logger.getAnonymousLogger().log(Level.SEVERE,"Error");
+
+
+                    }
+                }
+        );
+        MySingleton.getInstance(mContext).addToRequestQueue(jor);
+    }
+
 }
